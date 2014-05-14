c975d755cf113d6702c8111831d4642af8a33d9e
Oliver Braun <ob@obraun.net>
2014-05-14 14:04:34 +0000

    initial commit for Blatt05

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,29 @@
--- | Main entry point to the application.
 module Main where
 
--- | The main entry point.
+import           CodeGen
+import           IR
+import           Parser
+import           Scanner
+
 main :: IO ()
 main = do
-    putStrLn "Welcome to FP Haskell Center!"
-    putStrLn "Have a good day!"
+    -- lese den Inhalt der Datei "test.md" als einen kompletten String ein
+    input <- readFile "test.md"
+    -- versuche den String zu scannen
+    let maybeTokens = scan input
+    putStrLn "Scanner output\n=============="
+    print maybeTokens
+    -- the parse
+    case maybeTokens of
+        Nothing -> putStrLn "scanner failed"
+        Just tokens -> do -- der Scanner war erfolgreich
+            -- versuche die Tokens zu parsen
+            let maybeAst = parse tokens
+            putStrLn "\nParser output\n============="
+            print maybeAst
+            case maybeAst of
+                Nothing -> putStrLn "parser failed"
+                Just ast -> do -- der Parser war erfolgreich
+                    putStrLn "\nGenerated HTML\n=============="
+                    -- generiere HTML und gebe es aus
+                    putStrLn $ generateHTML ast
